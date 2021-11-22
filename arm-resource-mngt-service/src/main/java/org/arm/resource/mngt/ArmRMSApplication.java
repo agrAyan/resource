608229dc633a556621569fa5450a764c255ff450
Ayan <ayanagrawal5007@gmail.com>
2021-11-22 15:30:27 +0530

    resources

---
+++ arm-resource-mngt-service/src/main/java/org/arm/resource/mngt/ArmRMSApplication.java
@@ -0,0 +1,32 @@
+package org.arm.resource.mngt;
+
+import org.arm.resource.mngt.service.CampaignService;
+import org.arm.resource.mngt.service.IProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ArmRMSApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmRMSApplication.class, args);
+	}
+
+	@Autowired
+	CampaignService cmpService;
+	
+	@Autowired
+	IProjectService projectService;
+	
+	@Override
+	public void run(String... args) throws Exception {
+	
+		cmpService.getAllCampaign().forEach(System.out:: println);
+		projectService.getAllProject().forEach(System.out:: println);
+	}
+}
